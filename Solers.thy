95bc722a0650a6d9683477238d77929f3c3d9bb4
Michal Charemza <michal@charemza.name>
2023-02-04 16:26:35 +0000

    feat: initial there exists proof

--- Solers.thy
+++ Solers.thy
@@ -18,4 +18,12 @@ proof -
   ultimately show "B \<and> A" ..
 qed
 
+theorem
+  assumes Pf: "\<exists> x. P(f x)"
+  shows "\<exists> y. P(y)"
+proof -
+  from Pf obtain x where "P(f x)" ..
+  thus "\<exists> y. P y" ..
+qed
+
 end
